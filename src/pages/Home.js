c9a696d960433424e475da4114af8eab9b1946fe
SpiceWeasel66 <nagengasteva@gmail.com>
2023-10-10 09:11:53 +0100

    flickering window lights

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Footer from "./Footer";
+import Title from "../media/jcfonttitle.png";
 import "./Home.css";
 
 function Home() {
   return (
-    <div>
-      <div className="homepage">THIS IS THR HOME PAGE</div>
-      <Footer />{" "}
+    <div className="home">
+      <div className="homepage">
+        <div className="herocontainer">
+          <div className="flickeringlights"> </div>
+          <div className="hero">
+            {" "}
+            <img
+              src={Title}
+              alt="title coming to find you"
+              className="titleimage"
+            ></img>{" "}
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
