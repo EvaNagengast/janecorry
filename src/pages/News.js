e0c04ce76fed5660320247b301046c8895416c0d
SpiceWeasel66 <nagengasteva@gmail.com>
2023-10-25 21:37:03 +0100

    news + responsive

--- src/pages/News.js
+++ src/pages/News.js
@@ -28,7 +28,7 @@ function News() {
     <div className="News">
       <div className="newspage">
         <h1 className="text-center">NEWS</h1>
-        <p className="m-5">
+        <p className="mt-3 m-1 m-sm-4 m-md-5 text-center">
           {" "}
           If you would like to be amongst the first to know more details, please
           get in touch with me through the mailing list by emailing me at
@@ -41,21 +41,24 @@ function News() {
           >
             janecorryauthor@gmail.com
           </a>
-          <button
-            onClick={() => {
-              navigate("../newsletter");
-            }}
-            className="btnLightDark px-3 py-1 reviewLink"
-          >
-            click here to find out more
-          </button>
+          <p className="mt-2">
+            {" "}
+            <button
+              onClick={() => {
+                navigate("../newsletter");
+              }}
+              className="btnLightDark px-3 py-1 reviewLink"
+            >
+              click here to find out more
+            </button>
+          </p>
         </p>
         <hr />
         {/* 
         NEWS 
          */}
-        <div className="row align-items-center m-auto newsOne">
-          <div className="col-6">
+        <div className="row m-auto p-1 p-sm-3   align-items-center newsOne">
+          <div className="col-12 col-md-6">
             <div className="row  m-auto">
               <div className="col-6">
                 <img
@@ -73,7 +76,7 @@ function News() {
               </div>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-12 col-md-6 text-center text-md-start pt-2 p-md-0">
             {" "}
             <p>
               I'm very excited that <i>Coming To Find You</i> is being published
@@ -89,23 +92,23 @@ function News() {
               </a>
               in Canada and the States on December 19.
             </p>
-            <p>These are the covers.</p>
+            <p className="mb-0">These are the covers.</p>
           </div>
         </div>
         <hr />
         {/* 
         NEWS TWO
          */}{" "}
-        <div className="row m-auto align-items-center newsThree">
-          <div className="col-6 text-end">
+        <div className="row m-auto p-1 p-sm-3 align-items-center newsThree">
+          <div className="col-12 col-md-6 text-center text-md-end">
             <img
               src={Newspaper}
               alt="newspaper review of Coming To Find You"
               className="w-50 img-fluid  newspapersnippet"
             />
           </div>
-          <div className="col-6 p-3">
-            <p className=" mt-3 newsTwo">
+          <div className="col-12 col-md-6 text-center text-md-start">
+            <p className="mt-sm-3 mb-0 newsTwo">
               I'm absolutely thrilled that <br />
               Coming To Find You <br />
               is in the July issue of
@@ -126,13 +129,13 @@ function News() {
         {/* 
         NEWS THREE
          */}{" "}
-        <div className="row m-auto align-items-center newsTwo">
-          <div className="col-4">
+        <div className="row m-auto  p-1 p-sm-3  align-items-center newsTwo">
+          <div className="col-12 col-md-5 col-lg-4 text-center text-md-end">
             {" "}
             <img
               src={wahosaudio}
               alt="coming to find you book cover"
-              className=" w-100 img-fluid d-inline px-2 ctfycover"
+              className=" w-100 img-fluid d-inline px-2 wahoscover"
             />{" "}
             <MediaController audio>
               <audio slot="media" src={audiobook}></audio>
@@ -144,11 +147,11 @@ function News() {
               </MediaControlBar>
             </MediaController>
           </div>
-          <div className="col-8 p-3">
-            <p className=" mt-3 newsTwo">
+          <div className="col-12 col-md-7 col-lg-8">
+            <p className=" mt-3 my-0 px-2  newsTwo text-center text-md-start">
               Do you like listening to novels rather than reading? Then you
               might enjoy the audio version of <i>We All Have Our Secrets</i>{" "}
-              <p className="mt-5">
+              <p className="mt-5 mb-0">
                 Listen to the audiobook on{" "}
                 <a
                   href="https://amzn.to/3Ris6EI"
@@ -172,7 +175,6 @@ SOCIALS
           <br /> Facebook, Twitter and Instagram. .newspage
         </p>
         <p className="text-center">
-          {" "}
           <a
             href="https://www.facebook.com/authorjanecorry/"
             alt="Jane Corrys Facebook account"
@@ -195,25 +197,35 @@ SOCIALS
             <RiInstagramLine className="contactlinkicon" />
           </a>
         </p>
-        <div className="d-flex justify-content-between mt-4">
-          <InstagramEmbed
-            url="https://www.instagram.com/p/CwIEH9JLzub/"
-            width={333}
-            captioned="false"
-          />
-
-          <InstagramEmbed
-            url="https://www.instagram.com/p/CvFR657oeQ1/"
-            width={333}
-            captioned="false"
-          />
-
-          <InstagramEmbed
-            url="https://www.instagram.com/p/CuIF8JDLp2p/"
-            width={333}
-            captioned="false"
-          />
+        {/*  */}
+        <div className="row m-auto">
+          <div className="col">
+            {" "}
+            <InstagramEmbed
+              url="https://www.instagram.com/p/CwIEH9JLzub/"
+              width={333}
+              captioned="false"
+              className="m-auto embed"
+            />
+          </div>
+          <div className="col">
+            <InstagramEmbed
+              url="https://www.instagram.com/p/CvFR657oeQ1/"
+              width={333}
+              captioned="false"
+              className="m-auto embed"
+            />
+          </div>{" "}
+          <div className="col">
+            {" "}
+            <InstagramEmbed
+              url="https://www.instagram.com/p/CuIF8JDLp2p/"
+              captioned="false"
+              className="m-auto embed"
+            />
+          </div>
         </div>
+        {/*  */}
       </div>
       <Footer />
     </div>
