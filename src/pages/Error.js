c8424676451d9cada5cc519f1e38e7b3c89cff4a
SpiceWeasel66 <nagengasteva@gmail.com>
2023-11-13 20:14:30 +0000

    Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,8 +4,15 @@ import "./Error.css";
 
 function Error() {
   return (
-    <div>
-      <div className="errorpage">ERORO</div>
+    <div className="Error">
+      <div className="errorpage">
+        <h1>
+         <a href="https://www.janecorryauthor.com/" alt="get to main page" className="linkLightBase">
+           Click here {" "}
+          </a> 
+          to return to main page
+        </h1>
+      </div>
       <Footer />
     </div>
   );
